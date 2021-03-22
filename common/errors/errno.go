ce0c44da4ac534559bff0e458a573319cbdfdc20
foxsugar-sanse <565584326@qq.com>
2021-03-22 23:33:15 +0800

    完成配置文件基本结构与文档

---
+++ common/errors/errno.go
@@ -0,0 +1,13 @@
+package errors
+
+import (
+	"net/http"
+)
+
+type Errno struct {
+	HttpCode int
+	Code 	 int
+	Message  string
+}
+
+var ErrSaveTomlFileNot = &Errno{HttpCode: http.StatusInternalServerError,Code: 10001,Message: "配置文件保存失败"}
