9297e5f312936725790da3ca7cd011d2420ed09b
foxsugar-sanse <565584326@qq.com>
2021-03-23 23:47:37 +0800

    新增日志

--- common/logger/log.go
+++ common/logger/log.go
@@ -1,27 +1,68 @@
 package logger
 
+import (
+	"fmt"
+	"github.com/mengzushan/bups/utils"
+	"log"
+	"os"
+)
 
-func StdDebugLog()  {
-
+type STD interface {
+	StdDebugLog(info string)
+	StdInfoLog(info string)
+	StdErrorLog(info string)
+	StdWarnLog(info string)
+	StdTraceLog(info string)
+	Close()
 }
 
-func StdInfoLog() {
-
+type Logger struct {
+	file 	*os.File
+	logger 	*log.Logger
 }
 
-func StdErrorLog() {
-
+func (l *Logger)StdDebugLog(info string)  {
+	l.logger.SetPrefix("[Debug]")
+	l.logger.Println(info)
 }
 
-func StdWarnLog()  {
-
+func (l *Logger)StdInfoLog(info string) {
+	l.logger.SetPrefix("[Info]")
+	l.logger.Println(info)
 }
 
-func StdTraceLog() {
-
+func (l *Logger)StdErrorLog(info string) {
+	l.logger.SetPrefix("[Error]")
+	l.logger.Fatal(info)
 }
 
-func std()  {
+func (l *Logger)StdWarnLog(info string)  {
+	l.logger.SetPrefix("[Warn]")
+	l.logger.Println(info)
+}
 
+func (l *Logger)StdTraceLog(info string) {
+	l.logger.SetPrefix("[Trace]")
+	l.logger.Println(info)
+}
+
+func (l *Logger)Close() {
+	err := l.file.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Std()  STD {
+	pathHead,_ := os.Getwd()
+	pathRuned := utils.PathRune()
+	file, _ := os.Open(pathHead + fmt.Sprintf("%slog%sapp.log", pathRuned, pathRuned))
+	l := log.New(file,"",log.Lshortfile)
+	l.SetFlags(log.LstdFlags)
+	var s STD = &Logger{
+		file: file,
+		logger: l,
+	}
+	return s
 }
 
