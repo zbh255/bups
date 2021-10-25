686790aca9a56a564efeb07f00fdba9eeda924cd
abingzo <565584326@qq.com>
2021-10-26 01:54:12 +0800

    修改了原common/conf,使得更好的兼容当前

---
+++ test/config_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"github.com/abingzo/bups/common/config"
+	"os"
+	"testing"
+)
+
+func TestReadConfig(t *testing.T) {
+	file, err := os.Open("./test_read.toml")
+	if err != nil {
+		panic(err)
+	}
+	cfg := config.Read(file)
+	t.Log(cfg.Plugin["backup"]["file_path"]["log"].(string))
+}
+
+func TestWriteConfig(t *testing.T) {
+	file, err := os.Open("./test_read.toml")
+	if err != nil {
+		panic(err)
+	}
+	cfg := config.Read(file)
+	writeFile,err := os.OpenFile("./test_write.toml",os.O_WRONLY,0777)
+	if err != nil {
+		panic(err)
+	}
+	if err := config.Write(writeFile,cfg);err != nil {
+		panic(err)
+	}
+}
