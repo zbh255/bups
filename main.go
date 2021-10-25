fa76d1890d72a5d6dff55a7e0ce99ebd309ea2d2
abingzo <565584326@qq.com>
2021-10-26 01:54:45 +0800

    在main.main新增插件调度的功能

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
-	"github.com/mengzushan/bups/app"
-	this "github.com/mengzushan/bups/common/error"
+	"fmt"
+	"github.com/abingzo/bups/common/config"
+	"github.com/abingzo/bups/common/plugin"
 	"os"
 )
 
+const PluginPath = "./plugins"
+const ConfigPath = "./config/config.toml"
+
 func main() {
-	// 检查是否有参数
-	if len(os.Args) > 1 {
-		argErr := app.DeCommandArgs()
-		if argErr != this.Nil {
-			os.Exit(2)
-		} else {
-			os.Exit(0)
+	// 处理错误
+	defer func() {
+		if err := recover();err != nil {
+			fmt.Print(err)
 		}
+	}()
+	// 注册插件
+	ctx := plugin.NewContext()
+	ctx.StdOut = os.Stdout
+	// 提供配置文件
+	cfg,err := os.OpenFile(ConfigPath,os.O_WRONLY|os.O_RDONLY|os.O_SYNC,0777)
+	if err != nil {
+		panic(err)
 	}
-	// 调用函数调用
-	app.MainDisPatch()
+	ctx.Conf = cfg
+	// 注册在配置文件中声明的插件
+	mainConf := config.Read(ctx.Conf).Main
+	fnTable := make(map[string]func(string))
+	for _, v := range mainConf.Install {
+		fnTable[v] = ctx.Register
+	}
+	// 优先注册备份插件
+	if fn,ok := fnTable["backup.so"];ok {
+		fn(PluginPath + "/backup.so")
+		delete(fnTable,"backup.so")
+	}
+	// 注册其他的插件
+	for k, v := range fnTable {
+		v(PluginPath + "/" + k)
+	}
+	// 启动初始化插件
+	ctx.SetState(plugin.Init)
+	// 并发处理参数，如果有插件需要，则交给该插件
 }
 
