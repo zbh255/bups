90b55b282d46da63f624cd8bcf6f6d8db1be6d6d
abingzo <565584326@qq.com>
2021-10-27 22:20:16 +0800

    将定义程序的通用环境放在path包描述

---
+++ common/path/path.go
@@ -0,0 +1,9 @@
+// Package path 存储应用程序使用的一些标准路径
+package path
+
+const (
+	PathConfigFile       = "./config/config.toml"
+	PathPluginFileFolder = "./plugins"
+	PathBackUpCache      = "./cache"
+	PathAppLocationLog   = "./log"
+)
