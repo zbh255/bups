91390ed3fc47e23f48f134f588f5c333001aaa02
zbh255 <565584326@qq.com>
2022-01-05 21:14:45 +0800

    为封装的日志组件提供out level支持

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -5,6 +5,15 @@ import (
 	"log"
 )
 
+// 日志等级的定义
+const (
+	INFO uint8 = iota
+	DEBUG
+	TRACE
+	ERROR
+	PANIC
+)
+
 /*
 	提供日志支持
 */
@@ -17,39 +26,58 @@ type Logger interface {
 	Panic(err error)
 }
 
-func New(write io.Writer, prefix string) Logger {
+func New(write io.Writer, level uint8) Logger {
 	return &LoggerImpl{
-		logger: log.New(write, "Error", log.LstdFlags),
-		prefix: prefix,
+		logger: log.New(write, "Error", log.LstdFlags | log.Lshortfile),
+		level:       level,
 	}
 }
 
 type LoggerImpl struct {
 	logger *log.Logger
-	prefix string
+	level uint8
+}
+
+func (l *LoggerImpl) checkLevel(level uint8) bool {
+	return l.level >= level
 }
 
 func (l *LoggerImpl) Debug(message string) {
-	l.logger.SetPrefix(l.prefix + ".Debug:")
+	if !l.checkLevel(DEBUG) {
+		return
+	}
+	l.logger.SetPrefix("[Debug]")
 	l.logger.Print(message)
 }
 
 func (l *LoggerImpl) Info(message string) {
-	l.logger.SetPrefix(l.prefix + ".Info:")
+	if !l.checkLevel(INFO) {
+		return
+	}
+	l.logger.SetPrefix("[Info]")
 	l.logger.Print(message)
 }
 
 func (l *LoggerImpl) Error(message string) {
-	l.logger.SetPrefix(l.prefix + ".Error:")
+	if !l.checkLevel(ERROR) {
+		return
+	}
+	l.logger.SetPrefix("[Error]")
 	l.logger.Print(message)
 }
 
 func (l *LoggerImpl) Trace(message string) {
-	l.logger.SetPrefix(l.prefix + ".Trace:")
+	if !l.checkLevel(TRACE) {
+		return
+	}
+	l.logger.SetPrefix("[Trace]")
 	l.logger.Print(message)
 }
 
 func (l *LoggerImpl) Panic(err error) {
-	l.logger.SetPrefix(l.prefix + ".Panic:")
+	if !l.checkLevel(PANIC) {
+		return
+	}
+	l.logger.SetPrefix("[Panic]")
 	l.logger.Panic(err)
 }
