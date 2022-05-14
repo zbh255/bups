3aead238579cb7bda8885cffd48cd54645826a97
zbh255 <565584326@qq.com>
2022-05-14 15:52:26 +0800

    修复Recovery插件New函数返回nil导致的测试代码报空指针的Bug

--- plugins/recovery/recovery.go
+++ plugins/recovery/recovery.go
@@ -2,6 +2,35 @@ package recovery
 
 import "github.com/abingzo/bups/common/plugin"
 
-func New() plugin.Plugin {
-	return nil
+type Recovery struct {
+
 }
+
+func New() plugin.Plugin {
+	return &Recovery{}
+}
+
+func (r *Recovery) Start(args []string) {
+	panic("implement me")
+}
+
+func (r *Recovery) Caller(single plugin.Single) {
+	panic("implement me")
+}
+
+func (r *Recovery) GetName() string {
+	return "recovery"
+}
+
+func (r *Recovery) GetType() plugin.Type {
+	panic("implement me")
+}
+
+func (r *Recovery) GetSupport() []uint32 {
+	panic("implement me")
+}
+
+func (r *Recovery) SetSource(source *plugin.Source) {
+	panic("implement me")
+}
+
